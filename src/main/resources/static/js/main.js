2303614fccf5880cda1f06d3efb89bac5ba824a1
Aditya Patil <contactadityapatil@yahoo.in>
2017-12-19 20:02:33 +0530

    Fix code quality issues.

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,46 +1,27 @@
-//on load, check if authenticated
-window.onload = function () {
-    checkAuth();
-    //get type for new user
-    $("#userModal").on('hide.bs.modal', function () {
-        var type = $('input[type=radio]:checked').val();
-        if (type === "teacher") {
-            //call /createTeacher API
-            $.post("/createTeacher", function () {
-                window.location = 'teacher/homepage.html';
-            });
-        }
-        else {
-            //call /createStudent API
-            $.post("/createStudent", function () {
-                window.location = 'student/homepage.html';
-            });
-        }
-    });
-};
+
 
 //check auth and type
 function checkAuth() {
     //check if user is authenticated
     var request = new XMLHttpRequest();
-    request.open('GET', "all/isUserAuth");
+    request.open("GET", "all/isUserAuth");
     request.onload = function () {
         var data = request.responseText;
         if (data === "true") {
             var userTypeRequest = new XMLHttpRequest();
-            userTypeRequest.open('GET', '/userType');
+            userTypeRequest.open("GET", "/userType");
             userTypeRequest.onload = function () {
                 //get the type
                 var type = userTypeRequest.responseText;
-                if (type === 'student') {
-                    window.location = 'student/homepage.html';
+                if (type === "student") {
+                    window.location = "student/homepage.html";
                 }
-                else if (type === 'teacher') {
-                    window.location = 'teacher/homepage.html';
+                else if (type === "teacher") {
+                    window.location = "teacher/homepage.html";
                 }
                 else {
                     //show a popup to select student or teacher
-                    $("#userModal").modal('show');
+                    $("#userModal").modal("show");
                 }
             };
             userTypeRequest.send();
@@ -50,3 +31,23 @@ function checkAuth() {
     request.send();
 }
 
+//on load, check if authenticated
+window.onload = function () {
+    checkAuth();
+    //get type for new user
+    $("#userModal").on("hide.bs.modal", function () {
+        var type = $("input[type=radio]:checked").val();
+        if (type === "teacher") {
+            //call /createTeacher API
+            $.post("/createTeacher", function () {
+                window.location = "teacher/homepage.html";
+            });
+        }
+        else {
+            //call /createStudent API
+            $.post("/createStudent", function () {
+                window.location = "student/homepage.html";
+            });
+        }
+    });
+};
