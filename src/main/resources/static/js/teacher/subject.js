3fbceaa090fc7aa4ab924bca94010fdb8771fa28
akshay kumbhare <kumbhare.akshay@gmail.com>
2018-02-18 15:28:39 +0530

    Student List Display.
    Signed-off-by: akshay kumbhare <kumbhare.akshay@gmail.com>

--- src/main/resources/static/js/teacher/subject.js
+++ src/main/resources/static/js/teacher/subject.js
@@ -41,8 +41,51 @@ function studentUpload() {
 
 }
 
+function displayStudent() {
+    //var formdata = new FormData();
+    //formdata.append("subjectName",getUrlParameter("name"));
+    $.ajax(
+        {
+            url: "/api/getStudents",
+            type: "GET",
+            data: {subjectName: getUrlParameter("name")},
+            //contentType: "application/json;charset=utf-8",
+            //dataType: "json",
+            success: function (data) {
+                var result = "";
+                for (var i = 0; i < data.length; i++) {
+                    result += "<li class=\"list-group-item\"><span>" + data[i].studentEmail + "</span></li>";
+                    console.log(data[i].studentEmail);
+                }
+                var listElement = $("#studentList");
+                listElement.empty();
+                listElement.append(result);
+
+            },
+            error: function (jqXHR) {
+                console.log(jqXHR);
+            }
+
+        }
+    );
+    /*$.getJSON("/api/getStudents", function (data) {
+        var studlist = "";
+        console.log("Display Students");
+        for (var i = 0; i < data.length; i++) {
+            studlist += "<li class=\"list-group-item\"><span>" + data[i].name + "</span></li>";
+        }
+        var listElement = $("#studentList");
+        listElement.empty();
+        listElement.append(studlist);
+    });*/
+}
+
+
 window.onload = function () {
     hideLogin();
     var studentUploadBtn = document.getElementById("studentUploadBtn");
     studentUploadBtn.addEventListener("click", studentUpload);
+
+    var displayStudentsBtn = document.getElementById("displayStudents");
+    displayStudentsBtn.addEventListener("click", displayStudent);
 };
